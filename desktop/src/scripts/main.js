6ed7f2a859d967c40c0aae4dd3cc987521cebc5e
MAKIO135 <makio135@gmail.com>
2019-02-13 06:41:42 +0100

    add theme.js support

--- desktop/src/scripts/main.js
+++ desktop/src/scripts/main.js
@@ -160,3 +160,7 @@ udp.on('message', msg => {
 })
 
 udp.bind(app.config.ORCA_PORT)
+
+const theme = new Theme({ background: '#000000', f_high: '#ffffff', f_med: '#777777', f_low: '#444444', f_inv: '#000000', b_high: '#eeeeee', b_med: '#72dec2', b_low: '#444444', b_inv: '#ffb545' })
+theme.install(document.body)
+theme.start()
