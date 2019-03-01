e0f6d20f881dd1ec9b990f48208c25468122218b
makio135 <radisson@MAC22219-6.local>
2019-03-01 15:40:08 +0100

    reflect localStorage usage instead of config.json in listener.js

--- listener.js
+++ listener.js
@@ -1,21 +1,24 @@
 const osc = require('./desktop/node_modules/node-osc')
 
-const config = require('./desktop/src/config.json')
+const hosts = [
+    '127.0.0.1:8000',
+    '127.0.0.1:12000'
+]
 
-config.hosts.forEach(host => {
+hosts.forEach(host => {
     const [ip, port] = host.split(':')
     const oscserver = new osc.Server(port, ip)
     console.log(`Started Listener OSC server: ${host}`)
     
     // Error
     oscserver.on('error', err => {
-      console.log(`OSC server:\n${err.stack}`);
-      oscserver.close();
+        console.log(`OSC server:\n${err.stack}`);
+        oscserver.close();
     })
     
     // Message
     oscserver.on('message', msg => {
-      console.log(`${host} ${msg}`)
+        console.log(`${host} ${msg}`)
     })
 })
 
